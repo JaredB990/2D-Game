534d2f1fe5d13515710664ecffaa43a602e657d6
James Zesiger <zesigeja@mail.gvsu.edu>
2026-02-18 22:04:08 -0500

    changed background img so it is easier to see, only front row of enemys shoot (exclue enemy infront of player for testing)

--- Galaga.cpp
+++ Galaga.cpp
@@ -11,9 +11,18 @@ int main(int argc, char** argv) {
     PlayerShip playerShip("Sprites\\Falcon.png",256, 920, 64, 64, true);
     for (int x = 128; x < 1024; x+=64) {
         for (int y = 128; y < 512; y+=64) {
-            auto* enemy = new EnemyShip("Sprites\\Lvl1Enemy.png",x, y, 48, 48, true, 1);
-            enemy->setOwned(true);
-            GameScene.addObject(enemy);
+            if (y == 448 && (x >300 ||x<200)){
+               auto* enemy = new EnemyShip("Sprites\\Lvl1Enemy.png",x, y, 48, 48, true, 1); 
+               enemy->setOwned(true);
+               GameScene.addObject(enemy);
+            }
+            else{
+                auto* enemy = new EnemyShip("Sprites\\Lvl1Enemy.png",x, y, 48, 48, false, 1);
+                enemy->setOwned(true);
+                GameScene.addObject(enemy);
+            }
+            
+            
         }
 
     }
